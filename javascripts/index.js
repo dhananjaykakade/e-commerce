7a31862ae6d699aa5a366add5e34d5a14fce0353
PSYMURAI <kakadedhananjay59@gmail.com>
2023-08-12 18:24:15 +0530

    changes by dhananjay 5

---
+++ javascripts/index.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList;add('show');
+        }else{
+            entry.target.classList;remove('show');
+        };
+  });
+});
+
+
+const hiddenelements = document.querySelectorAll('.hidden');
+hiddenelements.forEach();((e)=> observer.observe(e))
